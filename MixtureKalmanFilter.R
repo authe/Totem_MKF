8bbd5f28393a56184d76956d1502f26c63aba06d
authe <a.uthemann@gmail.com>
2023-10-12 12:39:25 -0400

    Update MixtureKalmanFilter.R
    Increased resampling from Mt to max(10*Mt, 10000)

--- MixtureKalmanFilter.R
+++ MixtureKalmanFilter.R
@@ -1,13 +1,14 @@
 # first created: 30 Apr 2023
-# last updated: 8 Oct 2023
+# last updated: 12 Oct 2023
 # author: Andreas Uthemann
 
-MKF <- function(y, types, models, ind_mod, crit_eff = 0.2) {
+MKF <- function(y, types, models, ind_mod, crit_eff = 0.2, crit_single = (1 - 1e-06)) {
 
   # Given intial draw of submitters types (types: M*S matrix with types[m,s]=1 implying submitter s in draw m is weak type) 
   # and corresponding state space models (models: ind.mod[m] gives the correct model for type draw m
   # MFK returns log-likelihood (LL) and posterior for states (at,Pt) and types (ln.wt) for types having observed data y
-  # types are resampled at step t if effective sample size Mt.eff < crit.eff * Mt (i.e. final sample size Mt can be much smaller than initial type draw)
+  # types are resampled at step t if effective sample size Mt_eff < crit_eff * Mt (i.e. final sample size Mt can be much smaller than initial type draw)
+  # if posterior prob of a single type draw is close to 1 ( > 1 - crit_single), only keep that type going forward
 
   # output: list kf.val with length(kf.val) = number of types that "survived" resampling
   # kf.val[[m]]$LL : log-likelihood of final(!) type draw m given data y
@@ -22,17 +23,13 @@ MKF <- function(y, types, models, ind_mod, crit_eff = 0.2) {
   TT <- dim(y)[2]      # number of pricing periods
   S <- (dim(y)[1] - 1) # number of submitters
 
-  # resampling step if effective sample size falls below crit * (sample size)
-  crit <- crit_eff
-
   # only keep unique type draws and weigh resulting draws equally 
-  # (alternative: keep track of duplicates and reweigh corresponding draws -> computationally intensive for large M)
-  ind_mod <- ind_mod[!duplicated(types)]
   types <- unique(types)
+  ind_mod <- ind_mod[!duplicated(types)]
   # potentially reduced in size later via resampling (hence t in Mt)
   Mt <- dim(types)[1]
 
-  # list to keep track of ln.wt,LL,at,Pt
+  # list to keep track of ln.wt, LL, at, Pt
   kf_val <- vector(mode = "list", length = Mt)
 
   for (t in 1:TT) {
@@ -51,7 +48,8 @@ MKF <- function(y, types, models, ind_mod, crit_eff = 0.2) {
         kf_val[[m]]$types <- types[m, ]
       }
 
-      # rearrange submitters to fit state-space model
+      # rearrange submitters to fit state-space model:
+      # weak submitters first, then strong
       aux <- y[2:(S + 1), t]
       yt <- c(y[1, t], aux[as.logical(kf_val[[m]]$types)],
                        aux[!as.logical(kf_val[[m]]$types)])
@@ -79,50 +77,51 @@ MKF <- function(y, types, models, ind_mod, crit_eff = 0.2) {
     Mt_eff <- 1 / exp(ln_sum_wt2)
 
     # resample if effective sample is too small,
+    # else if 1 path has close to prob 1 -> continue with that path only
     # else only update log-weights to normalized log-weights
-    # (unless 1 path has close to prob 1 -> continue with that path only)
 
     wt_vec <- exp(ln_wt_vec)
-    crit_single <- 1 - 1e-05 # critical value to keep only single type draw
+    
+    if (Mt_eff / Mt < crit_eff) { # resample
+      
+      # makes sure we resample "enough" paths
+      aux <- sample(1:Mt, size = max(10*Mt, 10000), 
+                  replace = TRUE, prob = wt_vec) 
 
-    if ((Mt_eff / Mt < crit || max(wt_vec) > crit_single) && Mt > 1) {
+      new_paths <- sort(unique(aux))
+      new_weights <- sapply(new_paths, 
+                              function(p) sum(aux == p) / length(aux))
 
-      # if posterior weight of a path is close to 1 only keep that path
-      if (max(wt_vec) > crit_single) {
-
-        ind_max <- which(wt_vec == max(wt_vec))
-        Mt <- 1
-
-        # only keep type draw "ind"
-        kf_val <- kf_val[ind_max]
-        types <- types[ind_max, ]
-        ind_mod <- ind_mod[ind_max]
-        kf_val[[1]]$ln_wt <- 0
-
-      } else {  # resample
-
-        aux <- sample(1:Mt, size = Mt, replace = TRUE, prob = wt_vec)
-
-        new_paths <- sort(unique(aux))
-        Mt <- length(new_paths)
-
-        # only keep data for m in new.paths and update log-weights and
-        # model indices  (CAREFUL: paths get relabeled)
-        kf_val <- kf_val[new_paths]
-        types <- types[new_paths,]
-        ind_mod <- ind_mod[new_paths]
-
-        new_weights <- sapply(new_paths, function(p) sum(aux == p) / Mt)
-        for (m in 1:Mt) {
-          kf_val[[m]]$ln_wt <- log(new_weights[m])
+      # only keep data for m in new.paths and update log-weights and
+      # model indices  (CAREFUL: paths get relabeled)
+      Mt <- length(new_paths)
+      kf_val <- kf_val[new_paths]
+          
+      for (m in 1:Mt) {
+        kf_val[[m]]$ln_wt <- log(new_weights[m])
         }
-      }
 
-    } else {
+      types <- types[new_paths,]
+      ind_mod <- ind_mod[new_paths]
+      
+      } elseif (max(wt_vec) > crit_single && Mt > 1) { # keep 1 path only
+      
+      ind_max <- which(wt_vec == max(wt_vec))
+      Mt <- 1
+
+      # only keep type draw "ind_max"
+      kf_val <- kf_val[ind_max]
+      types <- types[ind_max, ]
+      ind_mod <- ind_mod[ind_max]
+      kf_val[[1]]$ln_wt <- 0
+      
+      } else {  # no resampling
+      
       for (m in 1:Mt) {
         kf_val[[m]]$ln_wt <- ln_wt_vec[m]
+        }
+
       }
-    }
 
   }  # end for loop for t in 1:TT
 
