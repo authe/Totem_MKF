11b64890edc2ea3a497299d9bc82aabc3d4c6f5d
authe <a.uthemann@gmail.com>
2024-01-23 16:54:37 -0500

    removed the reset to old seed on exit

--- MixtureKalmanFilter.R
+++ MixtureKalmanFilter.R
@@ -1,5 +1,5 @@
 # first created: 30 Apr 2023
-# last updated: 9 Nov 2023
+# last updated: 23 Jan 2024
 # author: Andreas Uthemann
 
 MKF <- function(y, types, models, ind_mod, crit_eff = 0.2,
@@ -16,9 +16,6 @@ MKF <- function(y, types, models, ind_mod, crit_eff = 0.2,
   # kf.val[[m]]$ln.wt : posterior log-weight of draw m given y
   # kf.val[[m]]at : posterior mean of state given y and m
   # kf.val[[m]]Pt : posterior variance of state given y and m
-
-  old <- .Random.seed
-  on.exit( { .Random.seed <<- old } ) # needed for MCMC
 
   set.seed(seed)  # used for resampling
   library(matrixStats)  # only used for logExpSum function (can be hand-coded if package does not work, e.g. https://gregorygundersen.com/blog/2020/02/09/log-sum-exp/)
