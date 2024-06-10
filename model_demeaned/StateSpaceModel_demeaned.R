0a9532881b941ff24bf8bef1141f7de4d508cecf
authe <a.uthemann@gmail.com>
2024-06-10 18:45:28 +0200

    fixed indexing error for Zt

--- model_demeaned/StateSpaceModel_demeaned.R
+++ model_demeaned/StateSpaceModel_demeaned.R
@@ -3,23 +3,23 @@ SP_model_demeaned <- function(rho, omega, sig_u, sig_e, sig_n, ord, S, W, sig_z=
   #########################  CREATE STATE SPACE MODEL FOR FKF PACKAGE ########################################
   
   # first created: 10 Apr 2024
-  # last updated: 10 Apr 2024
+  # last updated: 10 Jun 2024
   # author: Andreas Uthemann
   
-  # (i) bring model into state space form for FKF package Kalman filter
+  # (i) bring model into state space form for Kalman Filter
   # alpha_t = T alpha_{t-1} + H eps_t, eps_t ~ N(0,I) where eps_t = (u_t, e_t, eta_{1,t},...eta{W,t})'
   # y_t = Z alpha_t + G gamma_t , gamma_t ~ N(0,I)
   # Let i=1,...,W be the "weak" submitters with W <= S (number of submitters)
   # Then alpha_t = (theta_t^(0), ..., theta_t^(ord), x_{1,t}^(1),...,x_{1,t}^(ord), ..., x_{W,t}^(ord), e_t)
   # where x_{i,t}^(l) = theta_{i,t}^(l) - theta_t^(l) and 1 <= l <= ord.
-  # (ii) raw submission data is demean with cross-sectional mean
+  # (ii) raw submission data is demeaned with cross-sectional mean
   # weak submitters: s_{i,t} = a_t + theta_t^(1) + x_{i,t},
-  # strong submitters: s_{i,t} = a_t + theta_t^(0) + z_{i,t},
+  # strong submitters: s_{i,t} = a_t + theta_t^(0) + sig_z z_{i,t},
   # cross-section mean m_t = a_t + (1-omega) theta_t^(0) + omega theta_t^(1)
   # consensus price (lagged) p_t = m_{t-1} + sig_e e_t
   # demeaned data:
-  # strong: y_{i,t} = s_{i,t} - m_t = omega (theta_t^(0) - theta_t^(1)) + z_{i,t},
-  # weak: y_{i,t} = s_{i,t} - m_t = x_{i,t} - (1-omega) (theta_t^(0) - theta_t^(1)),
+  # strong: y_{i,t} = submission_{i,t} - m_t = omega (theta_t^(0) - theta_t^(1)) + sig_z z_{i,t},
+  # weak: y_{i,t} = submission_{i,t} - m_t = x_{i,t} - (1-omega) (theta_t^(0) - theta_t^(1)),
   # consensus price (lagged): q_t = p_t - m_{t-1} = sig_e e_t
   # shape of data for estimation y_t = (consensus price (lagged), weak submitters, strong submitters):
   # y_t = (q_t, y_{1,t}, ...., y_{W,t}, y_{W+1,t}, ...., y_{S,t}}.
@@ -31,8 +31,8 @@ SP_model_demeaned <- function(rho, omega, sig_u, sig_e, sig_n, ord, S, W, sig_z=
   N <- aux$N
   Q <- aux$Q
   V <- aux$V
-  PP <- aux$PP  # used in initial condition P0 for fkf
-  SIG <- V %*% t(V) # used in initial condition P0 for fkf
+  PP <- aux$PP  # used in initial condition P0 for Kalman filter
+  SIG <- V %*% t(V) # used in initial condition P0 for Kalman filter
 
   # transition equation: alpha_t = dt + Tt alpha_{t-1} + Ht eps_t
   # drift
@@ -60,19 +60,19 @@ SP_model_demeaned <- function(rho, omega, sig_u, sig_e, sig_n, ord, S, W, sig_z=
   # observation matrix
   Zt <- matrix(0, nrow = (1 + S), ncol = (ord + 1 + (W * ord) + 1))
 
-  # demeaned consensus price q_t = sig.e e_t
+  # demeaned consensus price q_t = sig_e e_t
   Zt[1, ord + 1 + (ord * W) + 1] <- sig_e
 
   # weak submitters i=1,...,W submit x_{i,t}^(1) - (1-omega)(theta_t^(0)-theta_t^(1))
-  Zt[2:(W + 1), 2] <- -(1 - omega)
+  Zt[2:(W + 1), 1] <- -(1 - omega)
   Zt[2:(W + 1), 2] <- 1 - omega
   for (s in 1:W){
     Zt[(1 + s), (ord + 1 + (s - 1) * ord + 1)] <- 1
   }
-  # "strong" submitters i=W+1,...,S submit omega (theta_t^(0) - theta_t^(1)) + sig.z z_{i,t}
+  # "strong" submitters i=W+1,...,S submit omega (theta_t^(0) - theta_t^(1)) + sig_z z_{i,t}
   if (W < S){
     Zt[(W + 2):(S + 1), 1] <- omega
-    Zt[(W + 2):(S + 1), 1] <- -omega
+    Zt[(W + 2):(S + 1), 2] <- -omega
   }
 
   # shock variance matrix GGt = E(Gt e e' Gt') = Gt Gt'
