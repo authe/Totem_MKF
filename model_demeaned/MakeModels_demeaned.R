05cc49765ce68755c997223fd84bb405164bfe4d
authe <a.uthemann@gmail.com>
2024-06-10 22:03:00 +0200

    minor edits

--- model_demeaned/MakeModels_demeaned.R
+++ model_demeaned/MakeModels_demeaned.R
@@ -1,11 +1,11 @@
 # first created: 24 Apr 2024
-# last updated: 24 Apr 2024
+# last updated: 10 Jun 2024
 # author: Andreas Uthemann
 
 
 MakeModels <- function(y, types, paras, init, ord){
   
-  # MakeModels(y, types, paras, init, ord) states space models for type draw types
+  # MakeModels(y, types, paras, init, ord) states space models for type draw "types"
   # of order "ord" given parameters "paras" in list "models"
   # "init" provides (potentially data-driven) values to set reasonable priors (a0,P0) for the initial state 
   # models[[ind.mod[m]]] : state space model for draw m (vector ind_mod of length M maps draw m to corresponding state space model) 
@@ -17,7 +17,7 @@ MakeModels <- function(y, types, paras, init, ord){
   ord <- ord  # highest order of beliefs
   S <- dim(y)[1] - 1  # number of submitters
   
-  # "estimation" parameters
+  # model parameters
   rho <- paras[1]
   omega <- paras[2]
   sig_u <- paras[3]
@@ -27,7 +27,7 @@ MakeModels <- function(y, types, paras, init, ord){
   
   
   # data-based parameter values used to set "reasonable" data-driven priors (a0,P0) for the initial state 
-  # Important: paras.0 only depend on data y and do not change with type m or "estimation" parameters paras
+  # Important: paras_0 only depend on data y and do not change with type m or "estimation" parameters paras
   # init <- InitialParas(y)
   paras_0 <- init$par
   rho_0 <- paras_0[1]
@@ -40,14 +40,16 @@ MakeModels <- function(y, types, paras, init, ord){
   
   ###################################   CREATE STATE SPACE MODELS  ####################################
   
-  # create model for W.m weak submitters (unlike for type space (size 2^S), we need at most S different models as the ordering of types does not matter for models since data is rearranged accord to type vector [see below])
+  # create model for Wm weak submitters (unlike for type space (size 2^S), we need at most S different 
+  # models as the ordering of types does not matter for models since data is rearranged accord to type vector [see below])
   
   Wm <- rowSums(types)  # vector with number of weak submitters for M paths
-  size_models <- sort(unique(Wm))  # # figure out distinct values of W (# of weak submitters)
+  size_models <- sort(unique(Wm))  # figure out distinct values of W (no. of weak submitters)
   ind_mod <- sapply(Wm, function(x) match(x, size_models) )  # index of state space for draw m
   
-  models <- vector(mode = "list", length = length(size_models))  # list to contain with vectors/matrices of state space models (dt, Tt, HHt, ct, Zt, GGt), i.e. models[[ind.mod[m]]]$Tt
-  
+  # list to contain with arrays of state space models (dt, Tt, HHt, ct, Zt, GGt), i.e. models[[ind.mod[m]]]$Tt
+  models <- vector(mode = "list", length = length(size_models))
+
   for (i in 1:length(size_models)){
     w <- size_models[i]
     ss <- SP_model_demeaned(rho = rho, omega = omega, sig_u = sig_u, sig_e = sig_e, 
@@ -63,7 +65,7 @@ MakeModels <- function(y, types, paras, init, ord){
     aux <- rbind(aux, matrix(0, nrow = (w * ord), ncol = (1 + ord)))
     P0 <- cbind(aux, P0)  # covariance matrix for theta^(0:ord) and W submitters' deviations
     P0 <- cbind(P0, rep(0, ord + 1 + (w * ord)))
-    P0 <- rbind(P0, c(rep(0, ord + 1 + (w * ord)), sig_e_0)) # add covariance of price error e_t
+    P0 <- rbind(P0, c(rep(0, ord + 1 + (w * ord)), sig_e_0^2)) # add variance of price error e_t
     
     models[[i]] <- list(dt = ss$dt, Tt = ss$Tt, HHt = ss$HHt, ct = ss$ct,
                         Zt = ss$Zt, GGt = ss$GGt, W = w, a0 = a0, P0 = P0)
