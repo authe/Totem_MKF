d5fe772ef1474d5d1a5d0a7f9b56a3a9871b6bd9
authe <a.uthemann@gmail.com>
2024-06-10 19:08:22 +0200

    minor notation changes in comments

--- model_demeaned/createStateSpaceMat2_Lagged_pubpriv_heterogenous.R
+++ model_demeaned/createStateSpaceMat2_Lagged_pubpriv_heterogenous.R
@@ -10,8 +10,8 @@ StateSpaceMatLag <- function(ord, rho, omega, sig_u, sig_e, sig_n, tol=1e-15){
   # transition equation: theta_t = M_k theta_{t-1} + N_k w_t with w_t ~ N(0, I_2)
   # observation equation: y_t = D_{k,1} theta_t + D_{k,2} theta_{t-1} + R_w w_t + R_n eta_{j,t} with eta_{j,t} = N(0,1)
   # where theta_t = (theta_t^(0), theta_t^(1), ..., theta_t^(ord)) and ord >= 1
-  # y_{1,t} = theta_t + sig.n eta_{j,t} and y_{2,t} = (1-omega) theta_{t-1}^(0) + omega theta_{t-1}^(1) + sig.e e_t
-  # P_k = lim_t -> infty Var(theta_t) : stationary state covariance matrix
+  # y_{1,t} = theta_t^(0) + sig_n eta_{j,t} and y_{2,t} = (1-omega) theta_{t-1}^(0) + omega theta_{t-1}^(1) + sig_e e_t
+  # P_k = (lim_t -> infty) Var(theta_t) : stationary state covariance matrix
   # KK_k : stationary Kalman gain
   
     
@@ -22,17 +22,19 @@ StateSpaceMatLag <- function(ord, rho, omega, sig_u, sig_e, sig_n, tol=1e-15){
   # y_t = D1 theta_t + D2 theta_{t-1} + R (w_t eta_{j,t})'
   # (formulas used for Kalman filter derive from Nimark (2015) "A Low Dimensional Kalman Filter", Economic Letters)
   
-  # initialise consensus price signal at p_t = theta_{t-1}^(0) + sig.e w_{2,t}
+  # initialise consensus price signal at p_t = theta_{t-1}^(0) + sig_e w_{2,t}
   
-  # initial conditions for state equation: theta_t^(0) = rho theta_{t-1}^(0) + sig.u w_{1,t} 
+  # initial conditions for state equation
+  # theta_t^(0) = rho theta_{t-1}^(0) + sig_u w_{1,t} 
   A <- matrix(rho, nrow = 1)
   C <- matrix( c(sig_u, 0, 0), nrow = 1)
   
   # initial conditions for observation equation
+  # y_t = D_{k,1} theta_t + D_{k,2} theta_{t-1} + R_w w_t + R_n eta_{j,t}
   D1 <- matrix(c(1,0), nrow = 2)   
   D2 <- matrix(c(0,1), nrow = 2)
-  R_w <- matrix( c(0, 0, 0, sig_e), nrow = 2)
-  R_n <- matrix( c(sig_n, 0), nrow = 2 )
+  R_w <- matrix(c(0, 0, 0, sig_e), nrow = 2)
+  R_n <- matrix(c(sig_n, 0), nrow = 2)
   R <- cbind(R_w, R_n)
   
   # starting value covariance matrix
@@ -43,8 +45,6 @@ StateSpaceMatLag <- function(ord, rho, omega, sig_u, sig_e, sig_n, tol=1e-15){
   for (k in 1:ord){
     
     # update Kalman gain KK and stationary state covariance matrix P
-    
-    #tol <- 10^(-15)
     count_max <- 100
     
     test_convergence <- TRUE
@@ -93,7 +93,6 @@ StateSpaceMatLag <- function(ord, rho, omega, sig_u, sig_e, sig_n, tol=1e-15){
   }
   
   # update Kalman gain KK and stationary state covariance matrix P
-  #tol <- 10^(-15)
   count_max <- 100
   
   test_convergence <- TRUE
@@ -112,7 +111,7 @@ StateSpaceMatLag <- function(ord, rho, omega, sig_u, sig_e, sig_n, tol=1e-15){
   }
   
   # calculate matrices for individual deviations from theta_t
-  # x_(j,t) = Q_k x_(j,t-1) + V_k eta_(j,t)
+  # x_{j,t} = Q_k x_{j,t-1} + V_k eta_{j,t}
   
   Q <- A_old - KK_old %*% (D1_old %*% A_old + D2_old)
   V <- matrix(sig_n * KK_old[,1], ncol = 1)
