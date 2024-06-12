8e1d5752705b4aeba39f0a576ed0b034bd8e09cf
authe <a.uthemann@gmail.com>
2024-06-12 12:17:20 +0200

    minor edits

--- model_demeaned/test_MKF.R
+++ model_demeaned/test_MKF.R
@@ -1,14 +1,15 @@
 # first created: 11 Apr 2024
-# last updated: 24 Apr 2024
+# last updated: 12 Jun 2024
 # author: Andreas Uthemann
 
-rm(list=ls())
+rm(list = ls())
 
 library(mcmc)
 library(tictoc)
 
-source('SimulateData_demeaned.R')
-source('LogLike_MKF.R')
+source("SimulateData_demeaned.R")
+source("LogLike_MKF.R")
+source("LogLike_MKF_MCMC.R")
 source("InitialParasGuess_demeaned.R")
 source("DrawTypes.R")
 
@@ -17,11 +18,13 @@ set.seed(seed_sim)
 
 # ----------------------------- output paths ----------------------------------
 
-outfile <- paste0("results_mcmc_simdata_paraset1_scale6_seed", seed_sim, ".RData")
+outfile <- paste0("results_mcmc_simdata_paraset1_scale6_seed",
+                  seed_sim, ".RData")
 path_out <- "results/"
 
 # output file for posterior weights wT of MCMC
-path_types <- paste0(path_out, "types_mcmc_simdata_paraset1_scale6_seed", seed_sim, ".RData")
+path_types <- paste0(path_out, "types_mcmc_simdata_paraset1_scale6_seed",
+                     seed_sim, ".RData")
 
 # -------------------------- Simulate data for testing ------------------------
 
@@ -40,35 +43,40 @@ sig_z_sim <- 0.01
 paras_sim <- c(rho_sim, omega_sim, sig_u_sim, sig_e_sim, sig_n_sim, sig_z_sim)
 
 # draw W weak submitters out of S with i.i.d. prob omega_sim
-W <- sum(rbinom(S, 1, omega_sim))   
+W <- sum(rbinom(S, 1, omega_sim))
 # have a least one weak submitter (code works with 0)
-W <- ifelse(W > 0, W, 1)   
+W <- ifelse(W > 0, W, 1)
 
 # simulate data (consensus price and S submission time series)
-y_sim <- SimulateDataLagged(S = S, W = W, TT = TT, rho = paras_sim[1], 
-                            omega = paras_sim[2], sig_u = paras_sim[3], 
-                            sig_e = paras_sim[4], sig_n = paras_sim[5], 
-                            sig_z = paras_sim[6], ord = ord_sim, seed = seed_sim)
+y_sim <- SimulateDataLagged(S = S, W = W, TT = TT, rho = paras_sim[1],
+                            omega = paras_sim[2], sig_u = paras_sim[3],
+                            sig_e = paras_sim[4], sig_n = paras_sim[5],
+                            sig_z = paras_sim[6], ord = ord_sim,
+                            seed = seed_sim)
 
 # introduce missing values (optional)
-p_miss <- 0.0   # percent of missing observation per submitter
-miss <- rbinom(S*TT, 1, p_miss)
-miss <- matrix(as.logical(miss), nrow = S)
+p_miss <- 0   # percent of missing observation per submitter
+if (p_miss > 0) {
 
-for (s in 1:40){
-  y_sim[(s+1), miss[s, ]] <- NA
+  miss <- rbinom(S * TT, 1, p_miss)
+  miss <- matrix(as.logical(miss), nrow = S)
+
+  for (s in 1:40){
+    y_sim[(s + 1), miss[s, ]] <- NA
+  }
 }
 
-#save(y_sim, paras_sim, seed_sim, file = "data/simdata_paraset1_NAs.RData")
+#save(y_sim, paras_sim, seed_sim, file = "data/simdata_paraset1_NAs.RData") # nolint
 
 # data-driven guesses for parameters and submitter types
-init <- InitialParas(y_sim, paras0 = c(rho_sim, NA, sig_u_sim, NA, sig_n_sim, NA))
+init <- InitialParas(y_sim,
+                     paras0 = c(rho_sim, NA, sig_u_sim, NA, sig_n_sim, NA))
 
 # calculate log-likelihood at true parameters (mostly test)
 
 M_mkf <- 500 # types draws for MKF
 p_class <- 0.9
-crit_eff = 0.2
+crit_eff <- 0.2
 types_mkf <- DrawTypes(M_mkf, init, p_class)
 
 tic()
@@ -80,10 +88,6 @@ toc()
 
 # -------------------------------- MCMC estimation  ---------------------------
 
-# log-likelihood for MCMC
-# (fct ll_mcmc accepts unrestricted parameters; original parameters are scaled)
-source("LogLike_MKF_MCMC.R")
-
 # list to keep track of index of surviving types and corresponding weights
 types_mcmc <- list()
 save(types_mcmc, file = path_types)
@@ -91,11 +95,11 @@ save(types_mcmc, file = path_types)
 # parameters for Mixed Kalman Filter
 ord <- 2
 p_class <- 0.9
-crit_eff = 0.2
+crit_eff <- 0.2
 M <- 500  # number of draws from types space ( 2^S possible type combinations)
 
 # parameters for parameter rescaling 
-l <- 0  # lower bound for standard deviation parameters (sig.u, sig.e, sig.n, sig.z)
+l <- 0  # lower bound for standard deviation parameters (sig.u, sig.e, sig.n, sig.z) #nolint
 h <- 5 # upper bound
 
 # draw M types for MKF
@@ -103,28 +107,28 @@ types <- DrawTypes(M_mkf, init, p_class)
 
 # initial values for parameter estimation
 par_0 <- init$par
-par_0 <- c(log(par_0[1:2] / (1- par_0[1:2])), 
-           log((par_0[3:6] - l) / (h - par_0[3:6]))) 
+par_0 <- c(log(par_0[1:2] / (1 - par_0[1:2])),
+           log((par_0[3:6] - l) / (h - par_0[3:6])))
 
 # MCMC parameters
-scale_mcmc <- diag(6) 
-scale_mcmc[1,1] <- 0.2   # rho
-scale_mcmc[2,2] <- 0.1   # omega
-scale_mcmc[3,3] <- 0.02   # sig_u
-scale_mcmc[4,4] <- 0.04  # sig_e
-scale_mcmc[5,5] <- 0.025 # sig_n
-scale_mcmc[6,6] <- 0.005 # sig_z
+scale_mcmc <- diag(6)
+scale_mcmc[1, 1] <- 0.2   # rho
+scale_mcmc[2, 2] <- 0.1   # omega
+scale_mcmc[3, 3] <- 0.02   # sig_u
+scale_mcmc[4, 4] <- 0.04  # sig_e
+scale_mcmc[5, 5] <- 0.025 # sig_n
+scale_mcmc[6, 6] <- 0.005 # sig_z
 
-nbatch_mcmc <- 100
-burnin_mcmc <- 10
+nbatch_mcmc <- 10
+burnin_mcmc <- 1
 
 # ----------------- MCMC estimation and processing of results  ----------------
 
 tic()
-out_mcmc <- metrop(ll_mcmc, initial = par_0, nbatch = nbatch_mcmc, 
-                   scale = scale_mcmc, y = y_sim, init = init, types = types, 
-                   ord = ord, crit_eff = crit_eff, l = l, h = h, 
-                   path_types = path_types)
+out_mcmc <- metrop(ll_mcmc, initial = par_0, nbatch = nbatch_mcmc,
+                   scale = scale_mcmc, y = y_sim, init = init, types = types,
+                   ord = ord, crit_eff = crit_eff, l = l, h = h,
+                   path_types = "")
 toc()
 
 # acceptance rate of chain (ideally between 20% to 30%)
@@ -133,26 +137,26 @@ acc_prob <- out_mcmc$accept
 # results of chain ( size nbatch * 6)
 batch_mcmc <- out_mcmc$batch
 
-# chain of rescaled parameters (plot to check burn in phase and mixing of chain) 
-rho_mcmc <- exp(batch_mcmc[,1]) / (1 + exp(batch_mcmc[,1]))
-omega_mcmc <- exp(batch_mcmc[,2]) / (1 + exp(batch_mcmc[,2]))
-sig_u_mcmc <- (l + h * exp(batch_mcmc[,3])) / (1 + exp(batch_mcmc[,3]))
-sig_e_mcmc <- (l + h * exp(batch_mcmc[,4])) / (1 + exp(batch_mcmc[,4]))
-sig_n_mcmc <- (l + h * exp(batch_mcmc[,5])) / (1 + exp(batch_mcmc[,5]))
-sig_z_mcmc <- (l + h * exp(batch_mcmc[,6])) / (1 + exp(batch_mcmc[,6]))
+# chain of rescaled parameters (plot to check burn in phase and mixing of chain) #nolint
+rho_mcmc <- exp(batch_mcmc[, 1]) / (1 + exp(batch_mcmc[, 1]))
+omega_mcmc <- exp(batch_mcmc[, 2]) / (1 + exp(batch_mcmc[, 2]))
+sig_u_mcmc <- (l + h * exp(batch_mcmc[, 3])) / (1 + exp(batch_mcmc[, 3]))
+sig_e_mcmc <- (l + h * exp(batch_mcmc[, 4])) / (1 + exp(batch_mcmc[, 4]))
+sig_n_mcmc <- (l + h * exp(batch_mcmc[, 5])) / (1 + exp(batch_mcmc[, 5]))
+sig_z_mcmc <- (l + h * exp(batch_mcmc[, 6])) / (1 + exp(batch_mcmc[, 6]))
 
-paras_est <- c(mean(rho_mcmc[burnin_mcmc:nbatch_mcmc]), 
-               mean(omega_mcmc[burnin_mcmc:nbatch_mcmc]), 
+paras_est <- c(mean(rho_mcmc[burnin_mcmc:nbatch_mcmc]),
+               mean(omega_mcmc[burnin_mcmc:nbatch_mcmc]),
                mean(sig_u_mcmc[burnin_mcmc:nbatch_mcmc]),
-               mean(sig_e_mcmc[burnin_mcmc:nbatch_mcmc]), 
-               mean(sig_n_mcmc[burnin_mcmc:nbatch_mcmc]), 
+               mean(sig_e_mcmc[burnin_mcmc:nbatch_mcmc]),
+               mean(sig_n_mcmc[burnin_mcmc:nbatch_mcmc]),
                mean(sig_z_mcmc[burnin_mcmc:nbatch_mcmc]))
 
-paras_est_se <- c(sd(rho_mcmc[burnin_mcmc:nbatch_mcmc]), 
-                  sd(omega_mcmc[burnin_mcmc:nbatch_mcmc]), 
+paras_est_se <- c(sd(rho_mcmc[burnin_mcmc:nbatch_mcmc]),
+                  sd(omega_mcmc[burnin_mcmc:nbatch_mcmc]),
                   sd(sig_u_mcmc[burnin_mcmc:nbatch_mcmc]),
-                  sd(sig_e_mcmc[burnin_mcmc:nbatch_mcmc]), 
-                  sd(sig_n_mcmc[burnin_mcmc:nbatch_mcmc]), 
+                  sd(sig_e_mcmc[burnin_mcmc:nbatch_mcmc]),
+                  sd(sig_n_mcmc[burnin_mcmc:nbatch_mcmc]),
                   sd(sig_z_mcmc[burnin_mcmc:nbatch_mcmc]))
 
 results_mcmc <- list(out_mcmc, paras_sim, paras_est, paras_est_se,
